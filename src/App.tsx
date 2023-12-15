89d2e1ae916f1a953975c6f3e10f2aed722237f1
shuto kikai <yysskikai04@icloud.com>
2023-12-15 09:39:52 +0900

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
 import React from "react";
 
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return 
-  <div className="App">
-    <div>
-      <h2>TODO</h2>
-      <form onSubmit={() => {}}
-        <input type="text" 
-        onChange={() => {}} className="inputText"/>
-        <input type="submit" value="作成" className="submitButton"/>
-      </form>
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  type Todo = {
+    inputvalue: string;
+    id: number;
+    checked: boolean;
+  };
+  return (
+    <div className="App">
+      <div>
+        <h2>TODO</h2>
+        <form onSubmit={() => {}}>
+          <input type="text" onChange={() => {}} className="inputText" />
+          <input type="submit" value="作成" className="submitButton" />
+        </form>
+      </div>
     </div>
-  </div>;
+  );
 }
 
 export default App;
