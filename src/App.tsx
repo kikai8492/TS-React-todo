10b85c545a28e8a14b3c5c1ab9b1ea0f2639574e
shuto kikai <yysskikai04@icloud.com>
2023-12-15 09:33:05 +0900

    input

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return 
+  <div className="App">
+    <div>
+      <h2>TODO</h2>
+      <form onSubmit={() => {}}
+        <input type="text" 
+        onChange={() => {}} className="inputText"/>
+        <input type="submit" value="作成" className="submitButton"/>
+      </form>
     </div>
-  );
+  </div>;
 }
 
 export default App;
